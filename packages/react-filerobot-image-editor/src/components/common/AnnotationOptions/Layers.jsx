adabd790b511f3c13d5f3c9d6d5f2c35e6fe5126
Ajit Kaur <ajit.kaur@baam.com>
2024-07-05 16:29:15 +0530

    add drag-drop on layers

--- packages/react-filerobot-image-editor/src/components/common/AnnotationOptions/Layers.jsx
+++ packages/react-filerobot-image-editor/src/components/common/AnnotationOptions/Layers.jsx
@@ -1,51 +1,107 @@
 /** External Dependencies */
-import React, { useMemo } from 'react';
+import React, { useState } from 'react';
 import { SELECT_ANNOTATION, SELECT_TOOL } from 'actions';
+import { useDrag, useDrop, DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 /** Internal Dependencies */
 import { useAnnotationEvents, useStore } from 'hooks';
 import { StyledIconWrapper } from './AnnotationOptions.styled';
 
+// Define a constant for the drag type
+const DRAG_TYPE = 'ANNOTATION';
+
+const LayerItem = ({ annotation, index, moveCard, setOn }) => {
+  const [{ isDragging }, dragRef] = useDrag({
+    type: DRAG_TYPE,
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
+  const [, dropRef] = useDrop({
+    accept: DRAG_TYPE,
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveCard(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  return (
+    <div
+      ref={(node) => dragRef(dropRef(node))}
+      style={{
+        opacity: isDragging ? 0.5 : 1,
+        backgroundColor: 'white',
+        cursor: 'move',
+        whiteSpace: 'nowrap',
+        overflow: 'hidden',
+        textOverflow: 'ellipsis',
+      }}
+      onClick={() => setOn(annotation.id, annotation.name)}
+    >
+      <StyledIconWrapper
+        style={{
+          width: "125px",
+          justifyContent: 'start',
+        }}
+      >
+        {annotation.name === "Image" ? "Image" : annotation.text}
+      </StyledIconWrapper>
+    </div>
+  );
+};
+
 const Layers = () => {
-    const { annotations = {}, selectionsIds = [], dispatch } = useStore();
+  const { annotations = {}, selectionsIds = [], dispatch } = useStore();
+  const [annotationss, setAnnotations] = useState(Object.values(annotations));
+  const annotationEvents = useAnnotationEvents();
 
-    const annotationEvents = useAnnotationEvents();
+  const setOn = (id, name) => {
+    const multiple = false;
 
-    const setOn = (id, name) => {
-        const multiple = false
+    dispatch({
+      type: SELECT_TOOL,
+      payload: {
+        toolId: name,
+        keepSelections: multiple,
+      },
+    });
 
-        dispatch({
-            type: SELECT_TOOL,
-            payload: {
-                toolId: name,
-                keepSelections: multiple,
-            },
-        });
+    dispatch({
+      type: SELECT_ANNOTATION,
+      payload: {
+        annotationId: id,
+        multiple,
+      },
+    });
+  };
 
-        dispatch({
-            type: SELECT_ANNOTATION,
-            payload: {
-                annotationId: id,
-                multiple,
-            },
-        });
-    };
+  const moveCard = (fromIndex, toIndex) => {
+    const updatedAnnotations = [...annotationss];
+    const [movedItem] = updatedAnnotations.splice(fromIndex, 1);
+    updatedAnnotations.splice(toIndex, 0, movedItem);
+    setAnnotations(updatedAnnotations);
+  };
 
-    return useMemo(
-        () =>
-            Object.values(annotations).map((annotation, index) => (
-                <>
-                    <StyledIconWrapper
-                        onClick={() => (
-                            setOn(annotation.id, annotation.name)
-                        )}
-                    >
-                        {`Layer ${index + 1}`}
-                    </StyledIconWrapper>
-                </>
-            )),
-        [annotations, annotationEvents, selectionsIds],
-    );
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div style={{ position: 'relative', overflowX: 'hidden' }}>
+        {annotationss.map((annotation, index) => (
+          <LayerItem
+            key={annotation.id}
+            annotation={annotation}
+            index={index}
+            moveCard={moveCard}
+            setOn={setOn}
+          />
+        ))}
+      </div>
+    </DndProvider>
+  );
 };
 
 export default Layers;
